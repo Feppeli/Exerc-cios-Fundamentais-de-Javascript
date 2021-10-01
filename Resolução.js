3e3282146d718f81cc62f048310a2fe427b9d39f
Felipe Teixeira <80359142+Feppeli@users.noreply.github.com>
2021-10-01 13:07:47 -0300

    Update Resolução.js

--- Resolução.js
+++ Resolução.js
@@ -28,19 +28,39 @@ console.log(saudacao())     // aqui nó imprimimos no console o resultado
  //3
 
 function calcularSalario(qntdHoras, slrHora) {      // função com os dois parâmetros passados
-    return `Seu salário é igual á: ${(qntdHoras * slrHora) }` // o retorno da função
+    var salario = qntdHoras * slrHora
+    var valorComDesconto = 70 / 100 *salario
+    return `Seu salário é de R$${valorComDesconto} já descontado o imposto`
 }
 
 console.log(calcularSalario(200, 12))
 
 //4
 
-var meses = {
-    1: 'Jan', 2: 'fev',
+function receberNomeDoMes(numero) {     // Criando uma função
+    const mapeamento = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', // criando uma Array 
+     'Setembro', 'Outubro', 'Agosto', 'Novembro', 'Dezembro']
+    return mapeamento[--numero] // devolvendo o valor da Array pelo numero começandopelo número 1
 }
 
-function mesSelecionado() {
-    var imprimirMes = meses[1],
-    return imprimirMes
+//OBS: Uma array começa na posição [0], então o [--numero] está fazendo iniciar na posição [1]
+
+console.log(receberNomeDoMes(11)) // imprimindo o mês Novembro
+
+5//
+
+function  maiorOuIgual(PrimeiroNumero, segundoNumero) {
+    if (PrimeiroNumero > segundoNumero) {
+        return `O primeiro valor é maior, sendo assim: ${PrimeiroNumero}>${segundoNumero}`
+    } else if(PrimeiroNumero === segundoNumero) {
+        return `O primeiro valor é igual ${PrimeiroNumero} = ${segundoNumero}`
+    }else{
+        return 'O primeiro valor é menor'
+    }
 }
-console.log(mesSelecionado())
+
+console.log(maiorOuIgual(12 ,3))
+console.log(maiorOuIgual(12, 12))
+console.log(maiorOuIgual(2,3))
+
+//6
